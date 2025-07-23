2538de6740dbe0b9a44245d5b5910c1082426918
Darshit javia <darshit.javia@simformsolutions.com>
2025-07-23 17:39:51 +0530

    edit script file

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,6 +3,10 @@ set -e
 
 echo "Installing system and Python dependencies..."
 sudo apt update -y
-sudo apt install -y python3-pip jq awscli
+sudo apt install -y python3-pip jq unzip curl
+
+echo "Installing AWS CLI via pip..."
+pip3 install --upgrade awscli
+
 cd /home/ubuntu/backend-app
 pip3 install -r requirements.txt
